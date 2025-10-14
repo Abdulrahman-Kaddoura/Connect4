f07764f72ab7d5951c0a9f7b036794e8ae4fad68
abd <akaddoura123@gmail.com>
2025-10-15 01:46:03 +0300

    changing the .sh file so that it recompiles on launch in the vm

--- start_connect4.sh
+++ start_connect4.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
 clear
-cd /root/Connect4_CMPS241project
-./connect
+
+# Go to the project folder
+cd /root/Connect4_CMPS241project || exit 1
+
+# Compile the source code every time
+gcc connect4.c -o connect
+
+# Check if compilation succeeded
+if [ $? -eq 0 ]; then
+    echo "Compilation successful. Running Connect 4..."
+    ./connect
+else
+    echo "Compilation failed. Please check connect4.c"
+fi
