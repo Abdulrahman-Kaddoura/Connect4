1eea521fc124a659f81d8e2cccd3fa76c36073d1
abd <akaddoura123@gmail.com>
2025-10-21 22:38:15 +0300

    fixing .sh file to work on alpine

--- start_connect4.sh
+++ start_connect4.sh
@@ -2,7 +2,7 @@
 clear
 
 # Go to the project folder
-cd /root/Connect4_CMPS241project || exit 1
+cd /root/Connect4 || exit 1
 
 # Compile the source code every time
 gcc -Wall -Werror -g Connect4.c main.c Helper.c -o connect
