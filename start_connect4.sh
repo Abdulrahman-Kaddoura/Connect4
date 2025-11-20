9e142a7bc1ed540357e532cd8fdcdc49b960995d
abd <akaddoura123@gmail.com>
2025-11-20 22:49:56 +0200

    fix: changed the names in .sh file

--- start_connect4.sh
+++ start_connect4.sh
@@ -5,12 +5,12 @@ clear
 cd /root/Connect4 || exit 1
 
 # Compile the source code every time
-gcc -Wall -Werror -g Connect4.c main.c Helper.c -o connect
+gcc -Wall -Werror -g Connect4.c main.c Helper.c -o connect4
 
 # Check if compilation succeeded
 if [ $? -eq 0 ]; then
     echo "Compilation successful. Running Connect 4..."
-    ./connect
+    ./connect4
 else
     echo "Compilation failed. Please check Connect4.c"
 fi
